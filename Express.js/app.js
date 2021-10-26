23f99d3dd7efed9d55df66e30016fdb671a46377
Rahul Goyal <rkgoyal007@gmail.com>
2021-10-26 13:01:54 +0530

    learned use of express routers

--- Express.js/app.js
+++ Express.js/app.js
@@ -3,20 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
- 
-app.use('/add-product',(req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.post('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/',(req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);

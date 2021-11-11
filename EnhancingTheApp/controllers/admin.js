41824148b5c5d986e9d2694fd49513597a6a9928
Rahul Goyal <rkgoyal007@gmail.com>
2021-11-11 12:53:42 +0530

    displayed the product data and added edit and delete button

--- EnhancingTheApp/controllers/admin.js
+++ EnhancingTheApp/controllers/admin.js
@@ -11,11 +11,11 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title; 
-  const imgUrl = req.body.imgUrl; 
-  const price = req.body.price; 
-  const description = req.body.description; 
-  const product = new Product(title, imgUrl, description, price);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };

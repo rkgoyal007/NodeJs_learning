1bb19cf3548918b8b08985e886c9d790d4973446
Rahul Goyal <rkgoyal007@gmail.com>
2021-11-11 17:02:23 +0530

    Loading product detail data

--- DynamicRoutesAndAdvancedModels/controllers/shop.js
+++ DynamicRoutesAndAdvancedModels/controllers/shop.js
@@ -10,9 +10,11 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.getProduct = (req, res, next) =>{
+exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log(prodId);
+  Product.findById(prodId, product => {
+    console.log(product);
+  });
   res.redirect('/');
 };
 

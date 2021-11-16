dce4fc68e9cd34cee9ce82ebf9ebd53fbdcca24e
Rahul Goyal <rkgoyal007@gmail.com>
2021-11-16 14:23:58 +0530

    added one to many relationship

--- Sequelize/models/user.js
+++ Sequelize/models/user.js
@@ -2,15 +2,15 @@ const Sequelize = require('sequelize');
 
 const sequelize = require('../util/database');
 
-const User = Sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: Sequelize.STRING,
+  email: Sequelize.STRING
 });
 
 module.exports = User;

b3ce3b65b3acbfb807e738b0c17920f0071ba837
Rahul Goyal <rkgoyal007@gmail.com>
2021-11-19 13:35:24 +0530

    Code added for storing the user in our database

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -19,13 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById(1)
+  User.findById('5baa2528563f16379fc8a610')
     .then(user => {
       req.user = user;
       next();
     })
     .catch(err => console.log(err));
-  next();
 });
 
 app.use('/admin', adminRoutes);

fe3068baef533a59fc5da4684c21e858fab2e66f
Rahul Goyal <rkgoyal007@gmail.com>
2021-12-03 14:49:34 +0530

    Setting a cookie

--- SessionAndCookies/controllers/auth.js
+++ SessionAndCookies/controllers/auth.js
@@ -1,4 +1,9 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req
+  .get('Cookie')
+  .split(';')[1]
+  .trim()
+  .split('=')[1];
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -8,5 +13,6 @@ exports.getLogin = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
     req.isLoggedIn = true;
+    res.setHeader('Set-Cookie','loggedIn=true'); 
     res.redirect('/');
   };
